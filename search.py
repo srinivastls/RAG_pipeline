413c707a05f745a565c57fc806b415dc08a06dfe
srinivastls <lakshmisrinivas365@gmail.com>
2025-04-25 15:27:31 +0530

    added hosted model

--- search.py
+++ search.py
@@ -2,6 +2,7 @@ from pymilvus import Collection, connections
 from database import connect_to_database, embed_text
 import requests
 import json
+import streamlit as st
 
 def search_database(query, tokenizer, model):
     collection = Collection("Chunked_Docs")
@@ -21,7 +22,7 @@ def search_database(query, tokenizer, model):
 
 def search_cloud_database(query, tokenizer, model):
     query_embedding = embed_text([query], tokenizer, model)
-
+    st.write("Query Embedding:", query_embedding)    
     url = "https://in03-505c80f9dc0263a.serverless.gcp-us-west1.cloud.zilliz.com/v2/vectordb/entities/search"
     headers = {
     "Authorization": "Bearer 1831fe961113fd3aa8af9c99b8a7d2f3e87b34bc2ac4d0f5bc701b1b09049be2aaba097631a7c8943701cb39b7f625597eb16bae",
