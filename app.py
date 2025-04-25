2a64d33170b1ffb3212b80a329c7a8370682acbd
srinivastls <lakshmisrinivas365@gmail.com>
2025-04-25 18:27:47 +0530

    added hosted model

--- app.py
+++ app.py
@@ -64,9 +64,9 @@ with tab3:
     query = st.text_input("Enter your query:")
     if st.button("Ask Legal AI") and query:
         finetuned_query = finetune_prompt(query, url)
-        print(f"Finetuned Query: {finetuned_query}")
         # chunks = search_database(finetuned_query, embedding_tokenizer, embedding_model)
         chunks = search_cloud_database(query, embedding_tokenizer, embedding_model)
+        st.write("Chunks:", chunks)
         # print(chunks)
         context = ""
         # for i, hit in enumerate(chunks[0]):
